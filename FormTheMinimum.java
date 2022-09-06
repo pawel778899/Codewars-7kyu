74d1457ccdd83dc5d3b17a5c2dae92f2ac2c4447
pawel778899 <106928352+pawel778899@users.noreply.github.com>
2022-09-06 10:49:23 +0200

    Update FormTheMinimum.java

--- FormTheMinimum.java
+++ FormTheMinimum.java
@@ -10,9 +10,6 @@ class Minimum {
     }
 }
 
-
-
-
 DESCRIPTION:
 Task
 Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
