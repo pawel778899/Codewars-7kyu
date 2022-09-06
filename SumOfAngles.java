56c4b4fbfb1e70c8a1c66f86febba015073e37a3
pawel778899 <106928352+pawel778899@users.noreply.github.com>
2022-09-06 10:47:57 +0200

    Create SumOfAngles.java

---
+++ SumOfAngles.java
@@ -0,0 +1,16 @@
+public class AngleSum {
+    public static int sumOfAngles(int n) {
+        return (n - 2) * 180;
+    }
+}
+
+
+
+
+
+
+
+DESCRIPTION:
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+GEOMETRY FUNDAMENTALS
