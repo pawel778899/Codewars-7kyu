fd2cdf861cf6f2437996465ab1165aac72e6345b
pawel778899 <pmaslow115@gmail.com>
2022-09-06 09:56:49 +0200

    first

---
+++ Minimum.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Minimum {
+
+    static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (a, b) -> a * 10 + b);
+    }
+}
