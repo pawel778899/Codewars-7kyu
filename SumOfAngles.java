92c9a908cff2754a60a594c1bf90e4d38e292788
pawel778899 <106928352+pawel778899@users.noreply.github.com>
2022-09-06 11:10:53 +0200

    Update SumOfAngles.java

--- SumOfAngles.java
+++ SumOfAngles.java
@@ -10,7 +10,7 @@ public class AngleSum {
 
 
 
-DESCRIPTION:
-Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+// DESCRIPTION:
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
 
-GEOMETRY FUNDAMENTALS
+// GEOMETRY FUNDAMENTALS
