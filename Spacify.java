d1904d0411b9345f8287ad8adf39a84d36cbba2e
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 14:51:34 +0200

    Create Spacify.java

---
+++ Spacify.java
@@ -0,0 +1,13 @@
+class Spacify {
+
+    static String spacify(String str) {
+        return str.replace("", " ").trim();
+    }
+}
+
+
+// DESCRIPTION:
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+// spacify("hello world") // returns "h e l l o   w o r l d"
+// FUNDAMENTALS STRINGS ARRAYS
