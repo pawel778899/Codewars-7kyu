48aebb4d3db36452b55909429e0c548facaa8252
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 09:08:23 +0200

    Create AllInclusive?.java

---
+++ AllInclusive?.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Rotations {
+    public static boolean containAllRots(String s, List<String> arr) {
+        return IntStream.range(0, s.length()).filter(i -> arr.indexOf(s.substring(i) + s.substring(0, i)) == -1).count() == 0;
+    }
+}
+
+
+
+
+
+// DESCRIPTION:
+// Input:
+
+// a string strng
+// an array of strings arr
+// Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+// a boolean true if all rotations of strng are included in arr
+// false otherwise
+// Examples:
+// contain_all_rots(
+//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+// contain_all_rots(
+//   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+// Note:
+// Though not correct in a mathematical sense
+
+// we will consider that there are no rotations of strng == ""
+// and for any array arr: contain_all_rots("", arr) --> true
+// Ref: https://en.wikipedia.org/wiki/String_(computer_science)#Rotations
+
+// FUNDAMENTALS STRINGS
