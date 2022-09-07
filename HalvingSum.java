d4138ee8f703f813e78003b0e98c736521b7ac7d
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 13:59:46 +0200

    Create HalvingSum.java

---
+++ HalvingSum.java
@@ -0,0 +1,18 @@
+public class HalvingSum {
+   int halvingSum(int n) {
+        return n != 0 ? n + halvingSum(n / 2) : 0;
+    }
+}
+
+
+
+DESCRIPTION:
+Task
+Given a positive integer n, calculate the following sum:
+
+n + n/2 + n/4 + n/8 + ...
+All elements of the sum are the results of integer division.
+
+Example
+25  =>  25 + 12 + 6 + 3 + 1 = 47
+ALGORITHMS
