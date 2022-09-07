05a0f422ee1028435661a8d38a0b7326f0c86f8e
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 13:59:02 +0200

    Create BumpsITheRoad.java

---
+++ BumpsITheRoad.java
@@ -0,0 +1,13 @@
+public class BumpsTheRoad {
+    public static String bumps(String road) {
+        return road.replaceAll("[^n]", "").length() > 15 ? "Car Dead" : "Woohoo!";
+    }
+}
+
+
+// DESCRIPTION:
+// Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+// FUNDAMENTAL SSTRINGS
