074cfc7eafcff211063ebd0637e28eb89069ad9a
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 11:11:57 +0200

    Create StringEndsWith?.java

---
+++ StringEndsWith?.java
@@ -0,0 +1,17 @@
+public class Kata {
+  public static boolean solution(String str, String ending) {
+    return str.endsWith(ending); 
+  }
+}
+
+
+
+
+// DESCRIPTION:
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+// STRINGSFUNDAMENTALS
