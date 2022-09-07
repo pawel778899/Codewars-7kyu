b0f9f7356968bc04c1f7c5ca5e5aca47e5100fc2
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 14:04:42 +0200

    Create SortArrays-1.java

---
+++ SortArrays-1.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class SortArray<TYPE> {
+    public static <TYPE> TYPE[] sortArray(TYPE[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static long[] sortArray(long[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static int[] sortArray(int[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static short[] sortArray(short[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static byte[] sortArray(byte[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static double[] sortArray(double[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static float[] sortArray(float[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+  }
+
+
+
+
+DESCRIPTION:
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+SORTING ARRAYS FUNDAMENTALS
