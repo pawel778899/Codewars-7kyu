a84c97acc38615e598bc251fbdcc69d4bd3db79f
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 14:34:08 +0200

    Create BeginnerSeries#3SumOfNumbers.java

---
+++ BeginnerSeries#3SumOfNumbers.java
@@ -0,0 +1,21 @@
+public class Sum {
+    public int GetSum(int a, int b) {
+        return a == b ? a : java.util.stream.IntStream.range(Math.min(a, b), Math.max(a, b) + 1).sum();
+    }
+}
+
+
+
+// DESCRIPTION:
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+// FUNDAMENTALS ALGORITHMS
