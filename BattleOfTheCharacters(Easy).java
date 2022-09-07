51c946da9e0198eafd9627a6c3e56dbf44da9f09
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 14:32:23 +0200

    Update BattleOfTheCharacters(Easy).java

--- BattleOfTheCharacters(Easy).java
+++ BattleOfTheCharacters(Easy).java
@@ -12,19 +12,19 @@ public class BattleOfTheCharacters {
   }
 
 
-DESCRIPTION:
-Description:
-Groups of characters decided to make a battle. Help them to figure out which group is more powerful. Create a function that will accept 2 strings and return the one who's stronger.
+// DESCRIPTION:
+// Description:
+// Groups of characters decided to make a battle. Help them to figure out which group is more powerful. Create a function that will accept 2 strings and return the one who's stronger.
 
-Rules:
-Each character have its own power: A = 1, B = 2, ... Y = 25, Z = 26
-Strings will consist of uppercase letters only
-Only two groups to a fight.
-Group whose total power (A + B + C + ...) is bigger wins.
-If the powers are equal, it's a tie.
-Examples:
-  battle("ONE", "TWO"); // => "TWO"`
-  battle("ONE", "NEO"); // => "Tie!"
-Related kata:
-Battle of the characters (Medium)
-ALGORITHMS
+// Rules:
+// Each character have its own power: A = 1, B = 2, ... Y = 25, Z = 26
+// Strings will consist of uppercase letters only
+// Only two groups to a fight.
+// Group whose total power (A + B + C + ...) is bigger wins.
+// If the powers are equal, it's a tie.
+// Examples:
+//   battle("ONE", "TWO"); // => "TWO"`
+//   battle("ONE", "NEO"); // => "Tie!"
+// Related kata:
+// Battle of the characters (Medium)
+// ALGORITHMS
