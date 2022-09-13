ea279605f8bdbd3ecb4edf0d88bb7e5faa6a6bcb
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 07:29:01 +0200

    Update ListFiltering.java

--- ListFiltering/ListFiltering.java
+++ ListFiltering/ListFiltering.java
@@ -1,7 +1,36 @@
 import java.util.List;
 
+
+// Solution 1
 interface Kata {
   static List<Object> filterList(List<Object> list) {
     return list.stream().filter(Integer.class::isInstance).toList();
   }
 }
+
+//Solution 2
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Kata {
+  
+  public static List filterList(final List<Object> list) {
+    return list.stream()
+      .filter(e -> e instanceof Integer)
+      .collect(Collectors.toList());
+  }
+
+}
+
+
+
+
+
+// DESCRIPTION:
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// Kata.filterList(List.of(1, 2, "a", "b")) => List.of(1,2)
+// Kata.filterList(List.of(1, 2, "a", "b", 0, 15)) => List.of(1,2,0,15)
+// Kata.filterList(List.of(1, 2, "a", "b", "aasf", "1", "123", 231)) => List.of(1, 2, 231)
+// LISTS FILTERING DATA STRUCTURES FUNDAMENTALS
