174a85e1cb64d143991187d373cb353b269ce89f
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 14:53:22 +0200

    Create 21SticksTest.java

---
+++ 21Sticks/21SticksTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void sampleTest() {
+        assertEquals(3, Kata.makeMove(3));
+    }
+}
