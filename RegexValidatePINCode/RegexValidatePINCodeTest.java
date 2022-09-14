95e6eb8ca2dc82077fc4db15a3ee063254badd49
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:17:23 +0200

    Create RegexValidatePINCodeTest.java

---
+++ RegexValidatePINCode/RegexValidatePINCodeTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void validPins() {
+        assertEquals(true, Solution.validatePin("1234"));
+        assertEquals(true, Solution.validatePin("0000"));
+        assertEquals(true, Solution.validatePin("1111"));
+        assertEquals(true, Solution.validatePin("123456"));
+        assertEquals(true, Solution.validatePin("098765"));
+        assertEquals(true, Solution.validatePin("000000"));
+        assertEquals(true, Solution.validatePin("090909"));
+    }
+    
+    @Test
+    public void nonDigitCharacters() {
+        assertEquals(false, Solution.validatePin("a234"));
+        assertEquals(false, Solution.validatePin(".234"));
+    }
+    
+    @Test
+    public void invalidLengths() {
+        assertEquals(false, Solution.validatePin("1"));
+        assertEquals(false, Solution.validatePin("12"));
+        assertEquals(false, Solution.validatePin("123"));
+        assertEquals(false, Solution.validatePin("12345"));
+        assertEquals(false, Solution.validatePin("1234567"));
+        assertEquals(false, Solution.validatePin("-1234"));
+        assertEquals(false, Solution.validatePin("1.234"));
+        assertEquals(false, Solution.validatePin("00000000"));
+    }
+}
