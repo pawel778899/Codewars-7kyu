9f06020a7090381eb7360c79a2eed6201e7d4e25
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 21:07:50 +0200

    Create  16+18=214Test.java

---
+++ 16+18=214/16+18=214Test.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class SillyAddTest {
+
+    @Test
+    public void RealAddition() {
+        System.out.println("****** Real addition ******");
+        assertEquals(13, SillyAdditon.add(2,11));
+        assertEquals(1, SillyAdditon.add(0,1));
+        assertEquals(0, SillyAdditon.add(0,0));
+    }
+
+
+    @Test
+    public void SillyAdditon() {
+        System.out.println("****** Silly addition******");
+        assertEquals(214, SillyAdditon.add(16,18));
+        assertEquals(515, SillyAdditon.add(26,39));
+        assertEquals(1103, SillyAdditon.add(122,81));
+    }
+  
+}
