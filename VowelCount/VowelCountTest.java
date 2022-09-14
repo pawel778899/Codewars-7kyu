62189267f0e85f4438062126f2d78dff464d6130
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:36:53 +0200

    Create VowelCountTest.java

---
+++ VowelCount/VowelCountTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class VowelsTest {
+    @Test
+    public void testCase1() {
+      assertEquals("Nope!", 5, Vowels.getCount("abracadabra"));
+      assertEquals("Nope!", 0, Vowels.getCount(""));
+      assertEquals("Nope!", 4, Vowels.getCount("pear tree"));
+      assertEquals("Nope!", 13, Vowels.getCount("o a kak ushakov lil vo kashu kakao"));
+      assertEquals("Nope!", 168, Vowels.getCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"));
+    }
+}
