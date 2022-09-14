09dc007e590f93e03c9dfb612fa0acd8bd7cc859
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 12:20:37 +0200

    Create ARuleOfDivisibilityBy7Test.java

---
+++ ARuleOfDivisibilityBy7/ARuleOfDivisibilityBy7Test.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class DivSevenTest {
+
+    @Test
+    public void test1() {
+        System.out.println("Basic Tests");
+        assertArrayEquals(new long[] {7, 2}, DivSeven.seven(1603));
+        assertArrayEquals(new long[] {35, 1}, DivSeven.seven(371));
+        assertArrayEquals(new long[] {42, 1}, DivSeven.seven(483));
+    }
+}
