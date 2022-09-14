160785e532f985454d693319999d87eab43934fd
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:18:14 +0200

    Create RemoveDuplicateWordsTest.java

---
+++ RemoveDuplicateWords/RemoveDuplicateWordsTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest{
+    @Test
+    public void basicTests(){
+        assertEquals("alpha beta gamma delta", Solution.removeDuplicateWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"));
+        assertEquals("my cat is fat", Solution.removeDuplicateWords("my cat is my cat fat"));
+    }
+}
