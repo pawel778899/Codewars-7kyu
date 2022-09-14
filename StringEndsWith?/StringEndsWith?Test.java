ab84b42798b0d07a640feeb9cf7f560361bccb08
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:26:47 +0200

    Create StringEndsWith?Test.java

---
+++ StringEndsWith?/StringEndsWith?Test.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void staticTests() {
+        check("samurai", "ai", true);
+        check("sumo", "omo", false);
+        check("ninja", "ja", true);
+        check("sensei", "i", true);
+        check("samurai", "ra", false);
+        check("abc", "abcd", false);
+        check("abc", "abc", true);
+        check("abcabc", "bc", true);
+        check("ails", "fails", false);
+        check("fails", "ails", true);
+        check("this", "fails", false);
+        check("this", "", true);
+        check(":-)", ":-(", false);
+        check("!@#$%^&*() :-)", ":-)", true);
+        check("abc\n", "abc", false);
+    }
+    
+    private static void check(String str, String ending, boolean expected) {
+      boolean result = Kata.solution(str, ending);
+      assertEquals("Expected solution(\"" + str + "\", \"" + ending + "\") to return " + expected, expected, result);
+    }
+}
