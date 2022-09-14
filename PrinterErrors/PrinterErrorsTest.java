9b091556a9e5ffb7495b31b2efcd6d08853c8376
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:16:39 +0200

    Create PrinterErrorsTest.java

---
+++ PrinterErrors/PrinterErrorsTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PrinterTest {
+    @Test
+    public void test() {
+        System.out.println("printerError Fixed Tests");
+        String s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+        assertEquals("3/56", Printer.printerError(s));
+    }
+}
