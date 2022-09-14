34949950497a3791d6e74cc972c78382bb95ddb5
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:33:35 +0200

    Create Testing1-2-3Test.java

---
+++ Testing1-2-3/Testing1-2-3Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+
+public class LineNumberingTest {
+    @Test
+    public void basicTests() {
+        assertEquals(Arrays.asList(), LineNumbering.number(Arrays.asList()));
+        assertEquals(Arrays.asList("1: a", "2: b", "3: c"), LineNumbering.number(Arrays.asList("a", "b", "c")));
+        assertEquals(Arrays.asList("1: ", "2: ", "3: ", "4: ", "5: "), LineNumbering.number(Arrays.asList("", "", "", "", "")));
+    }
+}
