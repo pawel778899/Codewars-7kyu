d2ea362df46dd3585c765523c83df1d531ee64d4
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:23:57 +0200

    Update and rename SortArrays-1.java to SortArrays-1/SortArrays-1.java

--- SortArrays-1/SortArrays-1.java
+++ SortArrays-1/SortArrays-1.java
@@ -40,7 +40,7 @@ public class SortArray<TYPE> {
 
 
 
-DESCRIPTION:
-Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+// DESCRIPTION:
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
 
-SORTING ARRAYS FUNDAMENTALS
+// SORTING ARRAYS FUNDAMENTALS
