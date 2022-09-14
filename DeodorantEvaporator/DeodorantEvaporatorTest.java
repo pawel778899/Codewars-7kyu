29fb0b074f693185cd73f67f05bcf66de54f4be9
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 12:35:58 +0200

    Create DeodorantEvaporatorTest.java

---
+++ DeodorantEvaporator/DeodorantEvaporatorTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class EvaporatorTest { 
+    @Test
+    public void testEvaporatorOne() {
+        assertEquals(22 , Evaporator.evaporator(10, 10, 10));     
+    }
+}
