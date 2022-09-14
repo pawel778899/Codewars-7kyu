6e9257f88714b097bc5f1cee44c6a39f570c2abe
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 12:35:07 +0200

    Create CovfefeTest.java

---
+++ Covfefe/CovfefeTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+
+public class CovfefeTest {
+    @Test
+    public void basicTest() {
+        assertEquals("covfefe", Covfefe.covfefe("coverage"));
+        assertEquals("covfefe covfefe", Covfefe.covfefe("coverage coverage"));
+        assertEquals("nothing covfefe", Covfefe.covfefe("nothing"));
+        assertEquals( "double space  covfefe" ,Covfefe.covfefe("double space "));
+        assertEquals("covfefe covfefe", Covfefe.covfefe("covfefe"));
+    }
+    
+}
