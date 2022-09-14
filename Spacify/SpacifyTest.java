3a363f88b0ac6dbd714f767eb8c663977f41c1af
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:25:12 +0200

    Create SpacifyTest.java

---
+++ Spacify/SpacifyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpacifyTest {
+    @Test
+    public void basicTest() {
+         assertEquals("h e l l o   w o r l d",Spacify.spacify("hello world"));
+         assertEquals("1 2 3 4 5",Spacify.spacify("12345"));
+    }
+}
