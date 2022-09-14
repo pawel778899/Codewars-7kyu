d85756fc2b72e92a015e840bf9f76164f1872efe
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:31:54 +0200

    Create SumOfTheFirstNthTermOfSeriesTest.java

---
+++ SumOfTheFirstNthTermOfSeries/SumOfTheFirstNthTermOfSeriesTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Test;
+
+
+public class NthSeriesTest {
+
+	@Test
+	public void test1() {
+		assertEquals("1.57", NthSeries.seriesSum(5));
+	}
+	@Test
+	public void test2() {
+		assertEquals("1.77", NthSeries.seriesSum(9));
+	}
+	@Test
+	public void test3() {
+		assertEquals("1.94", NthSeries.seriesSum(15));
+	}
+}
