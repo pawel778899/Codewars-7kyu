9778a43a04b586ce532108c57a71fb7125cc8545
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:12:55 +0200

    Update and rename OddOrEven?.java to OddOrEven?/OddOrEven?.java

---
+++ OddOrEven?/OddOrEven?.java
@@ -0,0 +1,39 @@
+public class Codewars {
+  public static String oddOrEven (int[] array) {
+ 
+    int sum=0;
+    for (int i=0; i<array.length; i++) {
+                  sum+=array[i];
+    }
+      if(sum%2==1){
+        return "odd";
+      }
+    else {
+      return "even";
+    }   
+  }
+}
+
+
+
+
+// DESCRIPTION:
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+// Have fun!
+
+// FUNDAMENTALS ARRAYS
