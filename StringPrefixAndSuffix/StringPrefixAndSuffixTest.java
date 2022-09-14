ef9535e886409295922602ed7d2cb326fb04ff08
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:27:47 +0200

    Create StringPrefixAndSuffixTest.java

---
+++ StringPrefixAndSuffix/StringPrefixAndSuffixTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SuffPrefTest{    
+    @Test
+    public void basicTests(){     
+        assertEquals(0,SuffPref.solve("abcd"));
+        assertEquals(1,SuffPref.solve("abcda"));    
+        assertEquals(3,SuffPref.solve("abcdabc"));
+        assertEquals(3,SuffPref.solve("abcabc"));   
+        assertEquals(1,SuffPref.solve("abcabca"));
+        assertEquals(0,SuffPref.solve("abcdabcc"));    
+        assertEquals(2,SuffPref.solve("aaaaa"));
+        assertEquals(2,SuffPref.solve("aaaa"));    
+        assertEquals(1,SuffPref.solve("aaa"));
+        assertEquals(1,SuffPref.solve("aa"));    
+        assertEquals(0,SuffPref.solve("a"));
+        assertEquals(0,SuffPref.solve("acbacc"));   
+    }
+}
