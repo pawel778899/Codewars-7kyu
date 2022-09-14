5545443e5d3982356ba4861900e672d7ba261a2e
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:23:00 +0200

    Update and rename SimpleRemoveDuplicates.java to SimpleRemoveDuplicates/SimpleRemoveDuplicates.java

--- SimpleRemoveDuplicates/SimpleRemoveDuplicates.java
+++ SimpleRemoveDuplicates/SimpleRemoveDuplicates.java
@@ -15,19 +15,19 @@ public class Solution {
   }
 
 
-DESCRIPTION:
-Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+// DESCRIPTION:
+// Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
 
-Example:
-For input: [3, 4, 4, 3, 6, 3]
+// Example:
+// For input: [3, 4, 4, 3, 6, 3]
 
-remove the 3 at index 0
-remove the 4 at index 1
-remove the 3 at index 3
-Expected output: [4, 6, 3]
+// remove the 3 at index 0
+// remove the 4 at index 1
+// remove the 3 at index 3
+// Expected output: [4, 6, 3]
 
-More examples can be found in the test cases.
+// More examples can be found in the test cases.
 
-Good luck!
+// Good luck!
 
-FUNDAMENTALS
+// FUNDAMENTALS
