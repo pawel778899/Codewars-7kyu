9f3dec48d1b66f4c1e9a2a426af3a68489e6c520
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:19:05 +0200

    Create ReverseWordsTest.java

---
+++ ReverseWords/ReverseWordsTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void exampleCases() {
+        assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god", Kata.reverseWords("The quick brown fox jumps over the lazy dog."));
+        assertEquals("elppa", Kata.reverseWords("apple"));
+        assertEquals("a b c d", Kata.reverseWords("a b c d"));
+        assertEquals("elbuod  decaps  sdrow", Kata.reverseWords("double  spaced  words"));
+    }
+}
