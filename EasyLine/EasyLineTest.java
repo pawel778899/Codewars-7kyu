4d99bdd452f2beee3015df65238d87d6236317e1
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 12:40:33 +0200

    Update EasyLineTest.java

--- EasyLine/EasyLineTest.java
+++ EasyLine/EasyLineTest.java
@@ -1,1 +1,18 @@
 
+import static org.junit.Assert.*;
+import java.math.BigInteger;
+import org.junit.Test;
+
+public class EasylineTest {
+
+    private static void testing(BigInteger actual, BigInteger expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        testing(Easyline.easyLine(7),  new BigInteger("3432"));
+        testing(Easyline.easyLine(13), new BigInteger("10400600"));
+        testing(Easyline.easyLine(17), new BigInteger("2333606220"));
+        testing(Easyline.easyLine(19), new BigInteger("35345263800"));
+    }
+}
