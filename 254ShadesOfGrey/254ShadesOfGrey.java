4882d77917db7a49585d91d3b52374312b2afa17
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-16 08:11:22 +0200

    Update 254ShadesOfGrey.java

--- 254ShadesOfGrey/254ShadesOfGrey.java
+++ 254ShadesOfGrey/254ShadesOfGrey.java
@@ -52,21 +52,21 @@ public class ShadesOfGrey {
   
   
   
-DESCRIPTION:
-Why would we want to stop to only 50 shades of grey? Let's see to how many we can go.
+// DESCRIPTION:
+// Why would we want to stop to only 50 shades of grey? Let's see to how many we can go.
 
-Write a function that takes a number n as a parameter and return an array containing n shades of grey in hexadecimal code (#aaaaaa for example). The array should be sorted in ascending order starting with '#010101', '#020202', etc. (using lower case letters).
+// Write a function that takes a number n as a parameter and return an array containing n shades of grey in hexadecimal code (#aaaaaa for example). The array should be sorted in ascending order starting with '#010101', '#020202', etc. (using lower case letters).
 
-Examples:
+// Examples:
 
-n =  1:    ["#010101"]
-n = 10:    ["#010101", "#020202", "#030303", "#040404", "#050505", "#060606", "#070707", "#080808", "#090909", "#0a0a0a"]
-As a reminder, the grey color is composed by the same number of red, green and blue: #010101, #aeaeae, or #555555.
+// n =  1:    ["#010101"]
+// n = 10:    ["#010101", "#020202", "#030303", "#040404", "#050505", "#060606", "#070707", "#080808", "#090909", "#0a0a0a"]
+// As a reminder, the grey color is composed by the same number of red, green and blue: #010101, #aeaeae, or #555555.
 
-Black: #000000 and white: #ffffff are not accepted values.
+// Black: #000000 and white: #ffffff are not accepted values.
 
-When n is negative, just return an empty array. If n is higher than 254, just return an array of 254 elements.
+// When n is negative, just return an empty array. If n is higher than 254, just return an array of 254 elements.
 
-Have fun!
+// Have fun!
 
-FUNDAMENTALS STRINGS ALGORITHMS
+// FUNDAMENTALS STRINGS ALGORITHMS
