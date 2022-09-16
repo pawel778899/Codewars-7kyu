428c8170fbb9bd32293113c3dab8d7947dff5358
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-16 21:04:57 +0200

    Create FindTheNextPerfectSquare!Test.java

---
+++ FindTheNextPerfectSquare!/FindTheNextPerfectSquare!Test.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+
+public class FindNextSquareTest {
+    @Test
+    public void test1() {
+        assertEquals(144, NumberFun.findNextSquare(121));
+    }
+    
+    @Test
+    public void test2() {
+        assertEquals(676, NumberFun.findNextSquare(625));
+    }
+    
+    @Test
+    public void test3() {
+        assertEquals(-1, NumberFun.findNextSquare(114));
+    }
+    
+    
+}
