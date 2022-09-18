b80311b16404f3d5fbc8c6909afd90be7e93a8b6
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-18 13:29:08 +0200

    Update ProductOfMaximumsOfArray(ArraySeries#2).java

--- ProductOfMaximumsOfArray(ArraySeries#2)/ProductOfMaximumsOfArray(ArraySeries#2).java
+++ ProductOfMaximumsOfArray(ArraySeries#2)/ProductOfMaximumsOfArray(ArraySeries#2).java
@@ -37,6 +37,21 @@ public class Solution {
   }
 }
 
+//Solution 4
+
+import java.util.Arrays;
+public class Solution
+        {
+            public static long maxProduct(int[] numbers, int sub_size)
+            {
+                Arrays.sort(numbers);
+                long result = 1;
+                for (int i = numbers.length-1;sub_size > 0;sub_size--,i--){
+                    result*=numbers[i];
+                }
+                return result; // Do your magic!
+            }
+        
 
 
 
